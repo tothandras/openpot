a5e78105161d6d3998260a91a008fa9fc1a64399
tothandras <andras.toth93@gmail.com>
2015-05-10 02:29:32 +0200

    no message

--- client/src/pots/pot/pot.controller.ts
+++ client/src/pots/pot/pot.controller.ts
@@ -8,14 +8,22 @@ module op.pots {
     class PotController implements IPotScope {
         pot: op.common.IPot;
         showDescription: boolean = false;
+        user: op.common.IUser;
 
         /* @ngInject */
-        constructor($log: ng.ILogService, public GravatarService: op.common.GravatarService) {
-            angular.extend(this, this.pot);
+        constructor($log: ng.ILogService,
+                    public GravatarService: op.common.GravatarService,
+                    public APIService: op.common.IAPIService) {
+
         }
 
         toggleDescription(): void {
             this.showDescription = !this.showDescription;
+            if (this.showDescription && !angular.isDefined(this.user)) {
+                this.APIService.getUserData(this.pot.cook).then((user: op.common.IUser) => {
+                    this.user = user;
+                });
+            }
         }
 
         gravatar(email: string): string {
